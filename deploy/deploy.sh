da5491901d6774eaa904bf7a8467abb16626c225
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2023-11-23 00:06:01 +0300

    feat: configure deployments
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

---
+++ deploy/deploy.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+set -eux
+
+# Create the namespace
+kubectl create namespace $DEPLOY_NAMESPACE || true
+
+# Delete Kubernetes secret if exists
+kubectl delete secret makao-service-account --namespace $DEPLOY_NAMESPACE || true
+
+# Create GCP service account file
+cat $GOOGLE_APPLICATION_CREDENTIALS >> ./service-account.json
+
+# Recreate service account file as Kubernetes secret
+kubectl create secret generic makao-service-account \
+    --namespace $DEPLOY_NAMESPACE \
+    --from-file=key.json=./service-account.json
+
+helm upgrade \
+    --install \
+    --debug \
+    --create-namespace \
+    --namespace="${DEPLOY_NAMESPACE}" \
+    --set app.name="${DEPLOY_RELEASE_NAME}" \
+    --set app.container.env.replicaCount="${REPLICA_COUNT}" \
+    --set app.container.env.dockerImage="${DOCKER_IMAGE}" \
+    --set app.container.env.googleCloudProjectID="${GOOGLE_CLOUD_PROJECT_ID}" \
+    --set app.container.env.db.databaseRegion="${DATABASE_REGION}" \
+    --set app.container.env.db.databaseInstance="${DATABASE_INSTANCE}" \
+    --set app.container.env.db.databaseInstanceConnectionName="${DATABASE_INSTANCE_CONNECTION_NAME}" \
+    --set app.container.env.db.postgresUser="${POSTGRES_USER}" \
+    --set app.container.env.db.postgresPassword="${POSTGRES_PASSWORD}" \
+    --set app.container.env.db.postgresHost="${POSTGRES_HOST}" \
+    --set app.container.env.db.postgresPort="${POSTGRES_PORT}" \
+    --set app.container.env.db.postgresDB="${POSTGRES_DB}" \
+    --set app.container.env.serviceName="${SERVICE_NAME}" \
+    --set app.container.env.db.rootCollectionSuffix="${ROOT_COLLECTION_SUFFIX}" \
+    --set app.container.env.debug="${DEBUG}" \
+    --set app.container.env.db.repository="${REPOSITORY}" \
+    --set app.container.env.aitAPIKey="${AIT_API_KEY}" \
+    --set app.container.env.aitUsername="${AIT_USERNAME}" \
+    --set app.container.env.aitSenderID="${AIT_SENDER_ID}" \
+    --set app.container.env.aitEnvironment="${AIT_ENVIRONMENT}" \
+    --set app.container.env.googleProjectNumber="${GOOGLE_PROJECT_NUMBER}" \
+    --set app.container.env.firebaseWebAPIKey="${FIREBASE_WEB_API_KEY}" \
+    --set app.container.env.oryxProviderChannel="${ORYX_PROVIDER_CHANNEL}" \
+    --set app.container.env.jwtSecret="${JWT_SECRET}" \
+    --set app.container.env.port="${PORT}" \
+    --set app.container.env.defaultResidenceId="${DEFAULT_RESIDENCE_ID}" \
+    --wait \
+    --timeout 300s \
+    -f ./charts/values.yaml \
+    $DEPLOY_RELEASE_NAME \
+    ./charts
