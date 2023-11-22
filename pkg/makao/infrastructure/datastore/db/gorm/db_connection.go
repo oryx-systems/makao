34ff21420eef19408aea51f8479a7711ae26126b
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2023-11-23 00:06:01 +0300

    feat: configure deployments
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

--- pkg/makao/infrastructure/datastore/db/gorm/db_connection.go
+++ pkg/makao/infrastructure/datastore/db/gorm/db_connection.go
@@ -3,10 +3,8 @@ package gorm
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/oryx-systems/makao/pkg/makao/application/common/helpers"
-
 	log "github.com/sirupsen/logrus"
 
 	// responsible for providing methods that are by gorm to connect to cloud SQL
@@ -20,14 +18,6 @@ import (
 )
 
 const (
-
-	// GoogleProject ...
-	GoogleProject = "GOOGLE_CLOUD_PROJECT_ID"
-	// DatabaseRegion ...
-	DatabaseRegion = "DATABASE_REGION"
-	// DatabasesInstance ...
-	DatabasesInstance = "DATABASE_INSTANCE"
-
 	// DBHost ..
 	DBHost = "POSTGRES_HOST"
 	// DBPort ...
@@ -41,15 +31,11 @@ const (
 )
 
 type connectionConfig struct {
-	host            string
-	port            string
-	user            string
-	password        string
-	dbname          string
-	project         string
-	region          string
-	instance        string
-	asCloudInstance bool
+	host     string
+	port     string
+	user     string
+	password string
+	dbname   string
 }
 
 // PGInstance box for postgres client. We use this instead of a global variable
@@ -68,70 +54,30 @@ func NewPGInstance() (*PGInstance, error) {
 	return pg, nil
 }
 
-// isLocalDB returns true if the service is currently configured to use a local
-// database.
-func isLocalDB() bool {
-	isLocal, err := strconv.ParseBool(os.Getenv("IS_LOCAL_DB"))
-	if err != nil {
-		return false
-	}
-
-	return isLocal
-}
-
 // startDatabase ...
 func startDatabase() *gorm.DB {
-	user := helpers.MustGetEnvVar(DBUser)
-	dbpassword := helpers.MustGetEnvVar(DBPASSWORD)
-	dbname := helpers.MustGetEnvVar(DBName)
-
-	var config connectionConfig
-	if isLocalDB() {
-		config.host = helpers.MustGetEnvVar(DBHost)
-		config.port = helpers.MustGetEnvVar(DBPort)
-		config.user = user
-		config.password = dbpassword
-		config.dbname = dbname
-	} else {
-		config.project = helpers.MustGetEnvVar(GoogleProject)
-		config.region = helpers.MustGetEnvVar(DatabaseRegion)
-		config.instance = helpers.MustGetEnvVar(DatabasesInstance)
-		config.asCloudInstance = true
-		config.user = user
-		config.password = dbpassword
-		config.dbname = dbname
+	config := connectionConfig{
+		host:     helpers.MustGetEnvVar(DBHost),
+		port:     helpers.MustGetEnvVar(DBPort),
+		user:     helpers.MustGetEnvVar(DBUser),
+		password: helpers.MustGetEnvVar(DBPASSWORD),
+		dbname:   helpers.MustGetEnvVar(DBName),
 	}
-
 	return boot(config)
 }
 
 func boot(cfg connectionConfig) *gorm.DB {
 	var err error
 	var db *gorm.DB
-	if cfg.asCloudInstance {
-		connString := fmt.Sprintf("host=%v:%v:%v user=%v dbname=%v password=%v sslmode=disable",
-			cfg.project, cfg.region, cfg.instance, cfg.user, cfg.dbname, cfg.password)
-		db, err = gorm.Open(postgres.New(postgres.Config{
-			DriverName: "cloudsqlpostgres",
-			DSN:        connString,
-		}), &gorm.Config{
-			PrepareStmt: true,
-			NamingStrategy: schema.NamingStrategy{
-				SingularTable: true,
-			},
-		})
 
-	} else {
-		// called when using localhost instance of postgres
-		connString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v", cfg.host, cfg.port, cfg.user, cfg.password, cfg.dbname)
-		db, err = gorm.Open(postgres.Open(connString), &gorm.Config{
-			PrepareStmt: true,
-			NamingStrategy: schema.NamingStrategy{
-				SingularTable: true,
-			},
-			Logger: logger.Default.LogMode(logger.Info),
-		})
-	}
+	connString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v", cfg.host, cfg.port, cfg.user, cfg.password, cfg.dbname)
+	db, err = gorm.Open(postgres.Open(connString), &gorm.Config{
+		PrepareStmt: true,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
 	if db == nil || err != nil {
 		log.Error(err)
